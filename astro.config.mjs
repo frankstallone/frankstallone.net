21b7d23f72b6241fcf275bc14e74ab1253d4193d
Frank Stallone <frank@designzen.co>
2024-06-09 10:55:46 -0400

    config: ⚙<fe0f> Adding aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), partytown(), sitemap()]
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    partytown(),
+    sitemap(),
+  ],
+  compilerOptions: {
+    baseUrl: ".",
+    paths: {
+      "@components/*": ["src/components/*"],
+      "@assets/*": ["src/assets/*"],
+    },
+  },
 });
